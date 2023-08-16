e2e0ee391eaee6b618fc0f2c621c726cf7bafcc9
Tiago Martins Speckart <75458110+tiagospeckart@users.noreply.github.com>
2023-08-16 20:01:53 -0300

    initial commit

---
+++ src/main/java/br/com/devstoblu/ravin/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.com.devstoblu.ravin;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RavinApplication.class);
+	}
+
+}
---
+++ src/test/java/br/com/devstoblu/ravin/RavinApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devstoblu.ravin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
